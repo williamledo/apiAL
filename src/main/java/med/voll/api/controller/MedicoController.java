612b60ddced9a40fadc5850f293a625fc50ca81b
williamledo <williamledoc@gmail.com>
2026-01-16 20:08:37 -0300

    adicionando exclusao logica

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -36,7 +36,7 @@ public class MedicoController {
 	
 	@GetMapping
 	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-		return repository.findAll(paginacao).map(DadosListagemMedico::new);
+		return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
 	}
 	
 	@PutMapping
@@ -52,7 +52,8 @@ public class MedicoController {
 	@Transactional
 	public void excluir(@PathVariable Long id) {
 		
-		repository.deleteById(id);		
+		var medico = repository.getReferenceById(id);
+		medico.excluir();
 	}
 	
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -38,8 +38,11 @@ public class Medico {
 	@Embedded
 	private Endereco endereco;
 
+	private Boolean ativo;
+	
 	public Medico(DadosCadastroMedico dados) {
 
+		this.ativo = true;
 		this.nome = dados.nome();
 		this.email = dados.email();
 		this.telefone = dados.telefone();
@@ -62,6 +65,11 @@ public class Medico {
 		if (dados.endereco() != null) {
 			this.endereco.atualizarInformacoes(dados.endereco());
 		}
+		
+	}
+
+	public void excluir() {
+		this.ativo = false;
 		
 	}
 	
